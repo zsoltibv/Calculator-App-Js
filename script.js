d50cc6d54d8bcfa4d93369c8333f144ef31475cb
zsoltibv <zsoltibv@gmail.com>
2023-01-07 12:39:58 +0200

    Possibility to calculate sum of n numbers and display result on calculator screen.

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
 const numberButtons = document.querySelectorAll("[data-number]");
+const operationButtons = document.querySelectorAll("[data-operation]");
+const equalButton = document.querySelector("[data-equals]");
+const deleteButton = document.querySelector("[data-delete]");
+const resultPanel = document.getElementById("result-panel");
 
 numberButtons.forEach((button) => {
-  console.log(button.innerHTML);  
   button.addEventListener("click", () => {
     console.log(button.innerHTML);
+    resultPanel.innerHTML += button.innerHTML;
   });
 });
+
+operationButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    resultPanel.innerHTML += button.innerHTML;
+  });
+});
+
+equalButton.addEventListener("click", () => {
+  let numbers = resultPanel.innerHTML.split("+");
+  let sum = 0;
+  for (const number in numbers) {
+    sum += parseInt(numbers[number]);
+  }
+  setResult(sum);
+});
+
+deleteButton.addEventListener("click", () => {
+  resultPanel.innerHTML = " ";
+});
+
+function setResult(result) {
+  resultPanel.innerHTML = result;
+}
