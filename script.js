42483eea72022aca06fbe50169f745443c56c99b
zsoltibv <zsoltibv@gmail.com>
2023-01-07 17:13:01 +0200

    Addition and substraction working correctly on any numbers

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const numberButtons = document.querySelectorAll("[data-number]");
 const operationButtons = document.querySelectorAll("[data-operation]");
 const equalButton = document.querySelector("[data-equals]");
-const deleteButton = document.querySelector("[data-delete]");
-const resultPanel = document.getElementById("result-panel");
+const stepBackButton = document.querySelector("[data-step-back]");
+const clearButton = document.querySelector("[data-clear]");
+const resultPanel = document.getElementById("result");
+let possibleOperands = ["+", "-"];
 
 numberButtons.forEach((button) => {
   button.addEventListener("click", () => {
-    console.log(button.innerHTML);
     resultPanel.innerHTML += button.innerHTML;
   });
 });
@@ -18,18 +19,42 @@ operationButtons.forEach((button) => {
 });
 
 equalButton.addEventListener("click", () => {
-  let numbers = resultPanel.innerHTML.split("+");
-  let sum = 0;
-  for (const number in numbers) {
-    sum += parseInt(numbers[number]);
+  let expression = resultPanel.innerHTML;
+  let operands = findOperands(expression);
+  let numbers = expression.split(/\+|\*|\-|\//);
+
+  console.log(operands);
+  console.log(numbers);
+
+  let sum = parseInt(numbers[0]);
+  for (i = 1; i < numbers.length; i++) {
+    if (operands[i - 1] === "+") {
+      sum += parseInt(numbers[i]);
+    } else if (operands[i - 1] === "-") {
+      sum -= parseInt(numbers[i]);
+    }
   }
   setResult(sum);
 });
 
-deleteButton.addEventListener("click", () => {
+stepBackButton.addEventListener("click", () => {
+  resultPanel.innerHTML = resultPanel.innerHTML.slice(0, -1);
+});
+
+clearButton.addEventListener("click", () => {
   resultPanel.innerHTML = " ";
 });
 
 function setResult(result) {
   resultPanel.innerHTML = result;
+}
+
+function findOperands(expression) {
+  let operands = [];
+  for (const char in expression) {
+    if (possibleOperands.includes(expression[char])) {
+      operands.push(expression[char]);
+    }
+  }
+  return operands;
 }
