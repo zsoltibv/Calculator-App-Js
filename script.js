91517d558efe2ef765b689029efe385d1c2f216c
zsoltibv <zsoltibv@gmail.com>
2023-01-10 01:42:27 +0200

    Made all buttons fully functional, implemented advanced options such as square, root, subunit calculation and sign changer. Polished design, fixed bugs.

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const numberButtons = document.querySelectorAll("[data-number]");
 const operationButtons = document.querySelectorAll("[data-operation]");
-const advancedOperationButtons = document.querySelectorAll("[data-operation-advanced]");
+const advancedOperationButtons = document.querySelectorAll(
+  "[data-operation-advanced]"
+);
 const equalButton = document.querySelector("[data-equals]");
 const stepBackButton = document.querySelector("[data-step-back]");
 const clearButton = document.querySelector("[data-clear]");
@@ -34,6 +36,41 @@ operationButtons.forEach((button) => {
       operationPanel.innerHTML += resultPanel.innerHTML;
       operationPanel.innerHTML += button.innerHTML;
       overwriteResultPanel = 1;
+    }
+  });
+});
+
+advancedOperationButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    deletePreviousEquationFromOperationPanel();
+    let num;
+    switch (button.innerHTML) {
+      case "1/x":
+        operationPanel.innerHTML += "1/" + resultPanel.innerHTML + "=";
+        setResult(1 / parseFloat(resultPanel.innerHTML));
+        break;
+      case "x^2":
+        operationPanel.innerHTML += resultPanel.innerHTML + "^2=";
+        num = parseFloat(resultPanel.innerHTML);
+        setResult(Math.pow(num, 2));
+        break;
+      case "rad 2":
+        operationPanel.innerHTML += resultPanel.innerHTML + "rad 2=";
+        num = parseFloat(resultPanel.innerHTML);
+        setResult(Math.sqrt(num));
+        break;
+      case "+/-":
+        if (resultPanel.innerHTML != "0") {
+          if (resultPanel.innerHTML[0] != "-") {
+            let sign = "-";
+            resultPanel.innerHTML = sign.concat(resultPanel.innerHTML);
+          } else {
+            resultPanel.innerHTML = resultPanel.innerHTML.slice(1);
+          }
+        }
+        break;
+      default:
+        break;
     }
   });
 });
@@ -93,13 +130,19 @@ function calculateResult(numbers, operands) {
   //calculate multiplication and division first
   for (i = 0; i < numbers.length; i++) {
     if (operands[i] === "x") {
-      numbers[i - 1] = numbers[i - 1] * numbers[i];
+      numbers[i - 1] = (numbers[i - 1] * numbers[i]).toString();
       operands.splice(i, 1);
       numbers.splice(i, 1);
+      i--;
+      console.log(operands);
+      console.log(numbers);
     } else if (operands[i] === "/") {
-      numbers[i - 1] = numbers[i - 1] / numbers[i];
+      numbers[i - 1] = (numbers[i - 1] / numbers[i]).toString();
       operands.splice(i, 1);
       numbers.splice(i, 1);
+      i--;
+      console.log(operands);
+      console.log(numbers);
     }
   }
   //calculate the rest of the operation
