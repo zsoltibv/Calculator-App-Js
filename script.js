cc704d4f476365238fb9a833b94771080bab421b
zsoltibv <zsoltibv@gmail.com>
2023-01-10 00:28:14 +0200

    Made multiplication and division operations exexute before other operations in a mixed operation.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const numberButtons = document.querySelectorAll("[data-number]");
 const operationButtons = document.querySelectorAll("[data-operation]");
+const advancedOperationButtons = document.querySelectorAll("[data-operation-advanced]");
 const equalButton = document.querySelector("[data-equals]");
 const stepBackButton = document.querySelector("[data-step-back]");
 const clearButton = document.querySelector("[data-clear]");
@@ -12,7 +13,7 @@ let nrOfEqualOperands = 0;
 
 numberButtons.forEach((button) => {
   button.addEventListener("click", () => {
-    if (resultPanel.innerHTML == "0") {
+    if (resultPanel.innerHTML == "0" && button.innerHTML != ".") {
       resultPanel.innerHTML = button.innerHTML;
     } else {
       if (overwriteResultPanel == 1) {
@@ -46,8 +47,8 @@ equalButton.addEventListener("click", () => {
   let numbers = expression.split(/\+|\=|\%|\x|\-|\//);
   numbers = numbers.filter(deleteEmptyValuesFromArray);
   //insert 0 if number starts with - sign
-  if(numbers[0] == ""){
-    numbers[0] = '0';
+  if (numbers[0] == "") {
+    numbers[0] = "0";
   }
 
   console.log(operands);
@@ -76,7 +77,7 @@ function setResult(result) {
 function findOperands(expression) {
   //set starting sign for first number(plus-minus)
   let operands = ["+"];
-  if(expression[0] == "-"){
+  if (expression[0] == "-") {
     operands = [];
   }
   for (const char in expression) {
@@ -89,15 +90,24 @@ function findOperands(expression) {
 
 function calculateResult(numbers, operands) {
   let sum = 0;
+  //calculate multiplication and division first
+  for (i = 0; i < numbers.length; i++) {
+    if (operands[i] === "x") {
+      numbers[i - 1] = numbers[i - 1] * numbers[i];
+      operands.splice(i, 1);
+      numbers.splice(i, 1);
+    } else if (operands[i] === "/") {
+      numbers[i - 1] = numbers[i - 1] / numbers[i];
+      operands.splice(i, 1);
+      numbers.splice(i, 1);
+    }
+  }
+  //calculate the rest of the operation
   for (i = 0; i < numbers.length; i++) {
     if (operands[i] === "+") {
       sum += parseFloat(numbers[i]);
     } else if (operands[i] === "-") {
       sum -= parseFloat(numbers[i]);
-    } else if (operands[i] === "/") {
-      sum /= parseFloat(numbers[i]);
-    } else if (operands[i] === "x") {
-      sum *= parseFloat(numbers[i]);
     } else if (operands[i] === "%") {
       sum %= parseFloat(numbers[i]);
       console.log(sum);
@@ -106,14 +116,14 @@ function calculateResult(numbers, operands) {
   return sum;
 }
 
-function deletePreviousEquationFromOperationPanel(){
+function deletePreviousEquationFromOperationPanel() {
   for (i = 0; i < operationPanel.innerHTML.length; i++) {
     if (operationPanel.innerHTML[i] == "=") {
-      operationPanel.innerHTML = operationPanel.innerHTML.slice(i + 1);  
+      operationPanel.innerHTML = operationPanel.innerHTML.slice(i + 1);
     }
   }
 }
 
-function deleteEmptyValuesFromArray(value ){
-  return (value != null && value !== false && value  !== "");
+function deleteEmptyValuesFromArray(value) {
+  return value != null && value !== false && value !== "";
 }
